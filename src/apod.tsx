f242a887c2dd8316e2936eccc0ab1210910c5b7e
Arhaan Ahmad <arhaan.ahmad2003@gmail.com>
2022-12-10 05:23:49 +0530

    Apod Works

---
+++ src/apod.tsx
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from 'react';
+
+function Apod(){
+    const [apod, setApod] = useState();
+    useEffect(() => {
+        fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+           .then((response) => response.json())
+           .then((data) => {
+              console.log(data);
+              setApod(data);
+           })
+           .catch((err) => {
+              console.log(err.message);
+           });
+     }, []);
+    
+     if (apod == undefined){
+        return(<></>);
+     }
+
+    return (<div className="text-center col-md-12 mt-3">
+  <h3 className="mb-4">Astronomy Picture Of The Day, By NASA</h3>
+  <div className="row">
+    <div className="col-lg-7">
+      {apod['media_type'] == 'video'? <iframe src="{{ apod_url }}" width="100%" height="100%" frameBorder={0} /> :  <img
+        src={apod['url']}
+        alt="Picture of the day"
+        className="img-fluid shadow-lg w-100 rounded-lg"
+      />}
+      
+      
+    </div>
+    <div className="col-lg-5">
+      <div className="d-none d-md-block apod_text_block rounded-lg p-3 shadow-lg bg-light">
+        <div className="container-fluid h1 cinzel">
+          {apod['title']}
+        </div>
+        <div className="font-weight-light text-left">
+          {apod['explanation']}
+        </div>
+        <div className="small text-right">
+          <b className="badge">
+            Credits:
+            <a
+              className="text-secondary"
+              href="https://apod.nasa.gov/apod/"
+              target="_blank"
+            >
+              APOD
+            </a>
+          </b>
+        </div>
+      </div>
+      <div className="d-md-none d-block card shadow-lg p-3 aboutCard">
+        <div className="container-fluid h3 font-weight-light cinzel">
+            {apod['title']}
+        </div>
+        <div className="lead small text-left">
+            {apod['explanation']}
+        </div>
+        <div className="small text-right">
+          <b className="badge">
+            Credits:
+            <a
+              className="text-secondary"
+              href="https://apod.nasa.gov/apod/"
+              target="_blank"
+            >
+              APOD
+            </a>
+          </b>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>)
+}
+
+export default Apod;
