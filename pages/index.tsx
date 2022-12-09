1d0bb932357ceae0874ec8b29fdb92adbd0b120f
Arhaan Ahmad <arhaan.ahmad2003@gmail.com>
2022-12-10 05:25:25 +0530

    Add the home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Script from "next/script";
 import styles from '../styles/Home.module.css'
 import Footer from '../src/footer'
+import Navbar from '../src/navbar'
+import Apod from '../src/apod';
+
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -12,61 +16,44 @@ export default function Home() {
         <meta charSet="utf-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
 
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        {/* <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin> */}
-        <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400&display=swap" rel="stylesheet"/>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossOrigin="anonymous"/>
-      <link href='https://fonts.googleapis.com/css?family=Cinzel' rel='stylesheet'/>
 {/* <link rel="stylesheet" href="{% static 'js/darkmode/dark-mode.css' %}"></link> */}
   {/* {% include 'includes/script_tags.html' %} */}
-  {/* {% include 'includes/script_mathjax.html' %} */}
-
-  {/* <link rel="stylesheet" href="{% static 'css/common.css' %}"> */}
-
-
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      <Navbar/>
+     
+      <main>
+      <div className="p-3" id="about">
+  <div className="container card my-md-4 my-3 p-3 pl-4 shadow-lg aboutCard">
+    <h1
+      className="display-4 text-center my-6"
+      style={{ paddingBottom: 10, paddingTop: 20 }}
+    >
+      Krittika: The Astronomy Club, IIT Bombay
+    </h1>
+    <p>
+      Who hasn’t been starstruck by the ethereal beauty of the midnight sky,
+      gazed at the moon’s comforting light and perhaps been thrilled at the
+      sight of a meteor flashing in the darkness of night? And have you ever
+      wished to be able to connect more deeply with all this celestial
+      magnificence? If yes, this is where your wish will come true.
+    </p>
+    <p>
+      Hey there! We are Krittika, the Astronomy club of IITB, and our prime
+      motive is to make the Universe more accessible to everyone. We conduct a
+      wide variety of activities - from stargazing sessions where you can gape
+      at the stunning detail in the moon and planets and stellar nebula our
+      telescopes show you (and even try photographing them on the way!), and
+      quizzes and group discussions where you rack your brains on mind-bending
+      topics - to offering projects to get a taste of astronomical research,
+      organizing trips to GMRT and screening evergreen movies on space tech.
+    </p>
+    <p>
+      Stay tuned on our social media handles to get regular updates on our
+      activities and participate in fun trivia quizzes too
+    </p>
+  </div>
+</div>
+<Apod/>
       </main>
       <Footer/>
     </div>
