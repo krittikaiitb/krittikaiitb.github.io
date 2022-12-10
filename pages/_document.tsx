2e57e0761c6f2e0f025f56d94f796664dd68b96e
Arhaan Ahmad <arhaan.ahmad2003@gmail.com>
2022-12-10 05:51:42 +0530

    Start using Script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,6 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
+
 import Footer from '../src/footer'
 import Navbar from '../src/navbar'
 
@@ -11,7 +13,7 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossOrigin="anonymous"></script>
+        <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossOrigin="anonymous"></Script>
 
       </body>
     </Html>
