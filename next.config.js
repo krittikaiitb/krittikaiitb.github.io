f6ecb59844186d4e8bba9acbc1ded804ce468e72
Arhaan Ahmad <arhaan.ahmad2003@gmail.com>
2022-12-12 06:52:18 +0530

    Try to setup github pages

--- next.config.js
+++ next.config.js
@@ -5,3 +5,15 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+// Required for setting up github pages
+if (process.env.NEXT_PUBLIC_APOD_KEY){
+  const repo = 'krittika-website'
+  const assetPrefix = `/${repo}/`
+  const basePath = `/${repo}`
+
+  module.exports = {
+    assetPrefix: assetPrefix,
+    basePath: basePath,
+  }
+}
