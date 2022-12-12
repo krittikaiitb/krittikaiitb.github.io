03fde4f1d9135d0bb3435f4453111a5d7bae5ac7
Arhaan Ahmad <arhaan.ahmad2003@gmail.com>
2022-12-12 07:57:46 +0530

    Fix the paths of two remaining images

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -100,7 +100,7 @@ export default function App({ Component, pageProps }: AppProps) {
       {/* <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/> */}
         <title>Krittika</title>
         <meta name="description" content="Astronomy club, IIT Bombay" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="favicon.ico" />
         <meta charSet="utf-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
 
