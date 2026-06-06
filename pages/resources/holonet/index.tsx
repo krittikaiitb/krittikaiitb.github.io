7200dfb16b7fd051ed2100d21abeea26ffa51bd5
Anisha7366 <anishakdash@gmail.com>
2026-06-06 23:50:35 +0530

    added modals to holonet.tsx

--- pages/resources/holonet/index.tsx
+++ pages/resources/holonet/index.tsx
@@ -1,36 +1,42 @@
 import Link from 'next/link';
-import PhotoAlbum from "react-photo-album";
-
-interface TalkProps{
-    name: string,
-    speaker: string,
-    link: string,
-    img?: string,
-}
+import React,{useState} from "react";
+import ModalBox, {TalkProps} from '../../../src/modal';
 
 var talks:TalkProps[] = [
-    {name: "HoloNet Talk 01", img: `/img/Astrophotography/leotriplet.png`, link: `/holonet_talks/Time_Domain_Astronomy.pdf`, speaker:"Introduction to Time Domain Astronomy"},
-    {name: "HoloNet Talk 02", img: `/img/Astrophotography/carina_nebula.png`, link: `/holonet_talks/Galactic_Centre.pdf`, speaker:"The Galactic Centre"}
+    {name: "HoloNet Talk 01", talkname:"Introduction to Time Domain Astronomy", img: `/img/Astrophotography/leotriplet.png`, link: `/holonet_talks/Time_Domain_Astronomy.pdf`, details:"This talk explores transients driven by compact objects - neutron stars and black holes, how their variability reveals physics under extreme conditions and realtime observing campaigns.", about:"Anirudh Salgundi is an Astrophysics researcher at the ATA Lab, University of North Carolina at Chapel Hill.  He was previously a research assistant at the STAR Lab, IIT Bombay. His current research focuses on fast transients, such as GRB's and electromagnetic counterparts of gravitational wave events.", date:"20 May, 2026"},
+    {name: "HoloNet Talk 02", talkname:"The Galactic Centre", img: `/img/Astrophotography/carina_nebula.png`, link: `/holonet_talks/Galactic_Centre.pdf`, details:"This talk discusses the black hole at our galaxy's center, which remains mysterious despite its proximity. We dive into findings on dust composition, elemental abundances, and constraints on nearby black holes, unveiling how supermassive black holes grow and shape their galactic environments.", about:"Tamojeet Roychowdhury is a first-year PhD student in Astronomy at the University of California, Berkeley with a B.Tech degree in Electrical Engineering from IIT Bombay. His research spans UV-optical-infrared observational astronomy, with current work on active galactic nuclei and high-redshift galaxies.", date:"2 June, 2026"}
 ]
 
-function TalkCard({name, link, speaker, img="/img/team/default_profile.png"}: TalkProps){
+interface TalkCardProps extends TalkProps{
+    onClick:()=>void;
+}
+
+function TalkCard({name, talkname, img="/img/team/default_profile.png", onClick, }: TalkCardProps){
     return(
-      <div className="col-md-4 mb-4">
-        <Link href={link} className="text-decoration-none text-reset">
-            <div className="card border-0 shadow bg-light">
-                <img src={img} className="card-img-top" alt="..."/>
+        <div className="col-md-4 mb-4">
+            <div onClick={onClick} className="card border-0 shadow bg-light cursor-pointer h-100">
+                <img src={img} className="card-img-top" alt="..."></img>
                 <div className="card-body text-center">
                     <h6 className="card-title mb-0">{name}</h6>
-                    <p className="card-text">{speaker}</p>
+                    <p className="card-text">{talkname}</p>
                 </div>
             </div>
-        </Link>
-      </div>
+        </div>
     )
 }
 
 export default function holonet() {
+
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    const [selectedTalk, setSelectedTalk] = useState<TalkProps | null>(null);
+    
+    const handleOpenModal = (talk: TalkProps) => {setSelectedTalk(talk); setIsModalVisible(true);};
+    const handleCloseModal = () => {setSelectedTalk(null); setIsModalVisible(false);};
+
     return(
+        <>
+        {isModalVisible && selectedTalk && (<ModalBox talk={selectedTalk} onClose={handleCloseModal}/>)}
+
         <div>
             <div className="mt-4" style={{maxWidth: "50rem", margin: "3rem auto 1rem", paddingLeft:"20px", paddingRight:"20px"}}>
                 <h1 style={{textAlign:"center"}}><b>HoloNet Talk Series</b></h1>
@@ -41,11 +47,12 @@ export default function holonet() {
                     <p className="display-4 pt-2 pb-2 text-center"></p>
                     <div className="container">
                         <div className="row row-cols-md-3 row-cols-2 justify-content-center">
-                            {talks.map((talk, key) => <TalkCard name={talk.name} link={talk.link} speaker={talk.speaker} img={talk.img} key={key}></TalkCard>)}
+                            {talks.map((talk, key) => <TalkCard key={key} {...talk} onClick={() => handleOpenModal(talk)}></TalkCard>)}
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+        </>
     )
 }
---
+++ src/modal.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import Link from 'next/link';
+
+export interface TalkProps {
+name: string;
+talkname: string;
+details: string;
+about: string;
+link: string;
+date: string;
+img?: string;
+}
+
+interface ModalProps {
+talk: TalkProps;
+onClose: () => void;
+}
+
+function ResourceCard({name, link, img="/img/team/default_profile.png"}: TalkProps){
+    return(
+      <div className="col-md-4 mb-4">
+        <Link href={link} target="_blank" className="text-decoration-none text-reset">
+            <div className="card border-0 shadow bg-light">
+                <img src={img} className="card-img-top" alt="..."/>
+                <div className="card-body text-center">
+                    <h6 className="card-title mb-0">{name}</h6>
+                </div>
+            </div>
+        </Link>
+      </div>
+    )
+}
+
+export default function ModalBox({ talk, onClose }: ModalProps) {
+return (
+    <div className="modal-overlay" onClick={onClose} style={{position: "fixed",inset: 0, zIndex: 1000,}}>
+        <div className="modal-content" onClick={(e)=>e.stopPropagation()}>
+            <div className="flex" style={{position:"fixed", top:"15vh", width:"50vw", zIndex:1, left:"25vw", overflow: "auto", backgroundColor:"#222"}}>
+                <div className="overflow-hidden" style={{ padding:"2px 16px"}}>
+                    <nav className="px-1 py-3">
+                        <button onClick={onClose} className="px-3 py-1 rounded" style={{float:"right"}}>✕</button>
+                    </nav>
+
+                    <div className="" style={{color:"white", font:""}}>
+                        <br></br>
+                            <h2 className="font-semibold" style={{textAlign:"center"}}>{talk.name}</h2>
+                            <h4 className="font-semibold" style={{textAlign:"center"}}>{talk.talkname}</h4>
+                            <p className="mt-2 font-bold" style={{float:"right", height:"10px"}}>Date of the Talk : {talk.date}</p>
+                        <br></br>
+
+                        <p style={{padding:"17px"}}>{talk.details}</p>
+
+                        <div style={{display:"flex",alignItems:"flex-start",gap:"1rem"}}>
+                            <ResourceCard name="slides" link={talk.link} img={talk.img}></ResourceCard>
+                            <div>
+                                <h6>About the speaker:</h6>
+                                <p>{talk.about}</p>
+                            </div>
+                        </div>
+
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+);
+}
