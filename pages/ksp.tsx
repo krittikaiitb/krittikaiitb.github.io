b58f18fb3af127ae09b82a40a7869f72aaa94f4e
Anisha7366 <anishakdash@gmail.com>
2026-06-20 11:19:22 +0530

    updated the ksp page

--- pages/ksp.tsx
+++ pages/ksp.tsx
@@ -1,144 +1,55 @@
 import Link from 'next/link';
-export default function KSP(){
+import PhotoAlbum from "react-photo-album";
+
+interface ResourceProps{
+    name: string,
+    link: string,
+    img?: string,
+}
+
+var resources:ResourceProps[] = [
+    {name: "KSP 6.0", img: `/img/ksp/geodesics.png`, link:`/KSP/6.0`},
+    {name: "KSP 5.0", img: `/img/ksp/agn.png`, link:`/KSP/5.0`},
+    {name: "KSP 4.0", img: `/img/ksp/grb.png`, link:`/KSP/4.0`},
+    {name: "KSP 3.0", img: `/img/ksp/cluster.png`, link:`/KSP/3.0`},
+    {name: "KSP 2.0", img: `/img/ksp/kirkwood.png`, link:`/KSP/2.0`},
+    {name: "KSP 1.0", img: `/img/ksp/analemma.png`, link:`/KSP/1.0`}
+]
+
+function ResourceCard({name, link, img="/img/team/default_profile.png"}: ResourceProps){
     return(
-        <div className="mt-4" style={{maxWidth: "50rem", margin: "3rem auto 6rem", paddingLeft:"20px",paddingRight:"20px"}}>
-            <h1><b>Krittika Summer Projects</b></h1>
-            <p>We run a range of exciting astrophysics projects through KSP every summer. This is one of the largest student led such programs in the country</p>
+      <div className="w-100 mb-4">
+        <Link href={link} className="text-decoration-none text-reset">
+            <div className="card border-0 shadow bg-light h-100 flex-row mx-auto" style={{width:"90%"}}>
+                <img src={img} className="card-img-top" alt="..." style={{width:"18%",objectFit:"cover"}}/>
+                <div className="card-body d-flex justify-content-center align-items-center">
+                    <h5 className="card-title mb-0">{name}</h5>
+                </div>
+            </div>
+        </Link>
+      </div>
+    )
+}
 
-            <br />
-            <h2>KSP 6.0</h2>
-            <p>Now in its sixth year, KSP has had one of the most interesting projects offered. You can take a look at all the project reports:</p>
-            <ul>
-                <li><h4><Link href="/KSPReports/2025/Imaging A Bright Neutron Star/End_term_report_Radio_imaging_final.pdf">Imaging A Bright Neutron Star</Link></h4></li>
-                <li><h4><Link href="/KSPReports/2025/Thermonuclear Bursts In Neutron Star X-Ray Binaries/Thermonuclear Bursts In Neutron Star Xray Binaries.pdf">Thermonuclear Bursts In Neutron Star XRay Binaries</Link></h4></li>
-                <li><h4>Visualizing Geodesics In Schwarzchild SpaceTime</h4></li>
-                <ul>
-                    <li><Link href="/KSPReports/2025/Visualizing Geodesics Through Schwarzchild SpaceTime/Aryan.pdf">Aryan&apos;s Report</Link></li>
-                    <li><Link href="/KSPReports/2025/Visualizing Geodesics Through Schwarzchild SpaceTime/Gayatri.pdf">Gayatri&apos;s Report</Link></li>
-                    <li><Link href="/KSPReports/2025/Visualizing Geodesics Through Schwarzchild SpaceTime/Rudra.pdf">Rudra&apos;s Report</Link></li>
-                    <li><Link href="public/KSPReports/2025/Visualizing Geodesics Through Schwarzchild SpaceTime/Vedant.pdf">Vedant&apos;s Report</Link></li>
-                </ul>
-                <li><h4>Gamma Ray Bursts - Theory And Analysis</h4></li>
-                <ul>
-                    <li><Link href="/KSPReports/2025/Gamma Ray Bursts - Theory And Analysis/Aditya.pdf">Aditya&apos;s Report</Link></li>
-                    <li><Link href="/KSPReports/2025/Gamma Ray Bursts - Theory And Analysis/Jahnavi.pdf">Jahnavi&apos;s Report</Link></li>
-                    <li><Link href="/KSPReports/2025/Gamma Ray Bursts - Theory And Analysis/Pratham.pdf">Pratham&apos;s Report</Link></li>
-                    <li><Link href="/KSPReports/2025/Gamma Ray Bursts - Theory And Analysis/Pratyush.pdf">Pratyush&apos;s Report</Link></li>
-                </ul>
-                <li><h4>Design And Analysis Of Radomes</h4></li>
-                <ul>
-                    <li><Link href="/KSPReports/2025/Design And Analysis Of Radomes/Aleena.pdf">Aleena&apos;s Report</Link></li>
-                    <li><Link href="/KSPReports/2025/Design And Analysis Of Radomes/Anvit.pdf">Anvit&apos;s Report</Link></li>
-                    <li><Link href="/KSPReports/2025/Design And Analysis Of Radomes/Manohara.pdf">Manohara&apos;s Report</Link></li>
-                    <li><Link href="/KSPReports/2025/Design And Analysis Of Radomes/Sandipan.pdf">Sandipan&apos;s Report</Link></li>
-                </ul>
-            </ul>
-            <h2>KSP 5.0</h2>
-            <p>Now in its fifth year, KSP had the largest variety of projects offered yet. You can take a look at all the project reports:</p>
-            <ul>
-                <li><h4><Link href="/KSPReports/2024/Eclipsing Binaries/KSP_5_0_EB_Project_Report.pdf">Eclipsing Binaries</Link></h4></li>
-                <li><h4><Link href="/KSPReports/2024/Hunt for the QCD axion dark matter using gravitational microlensing/KSP_Project_Report_DarkMatterAxionsPBH.pdf">Hunt for the QCD axion dark matter using gravitational microlensing</Link></h4></li>
-                <li><h4><Link href="/KSPReports/2024/Modifications of stellar structure equations due to Rotation and Tidal stretching/KSPReport.pdf">Modifications of stellar structure equations due to Rotation and Tidal stretching</Link></h4></li>
-                <li><h4><Link href="/KSPReports/2024/Prospects of LSST, ILMT and AXIS for searching SMBHB candidates/KSP_final.pdf">Prospects of LSST, ILMT and AXIS for searching SMBHB candidates</Link></h4></li>
-                <li><h4>Binary black holes from scratch</h4></li>
-                <ul>
-                    <li><Link href="/KSPReports/2024/Binary black holes from scratch/KSP_binary_black_holes.pdf">Arash&apos;s Report</Link></li>
-                    <li><Link href="/KSPReports/2024/Binary black holes from scratch/KSP_Project_Report.pdf">Arya&apos;s Report</Link></li>
-                    <li><Link href="/KSPReports/2024/Binary black holes from scratch/KSP_Report_Chaitanya_Divyesh_Patel.pdf">Chaitanya&apos;s Report</Link></li>
-                    <li><Link href="/KSPReports/2024/Binary black holes from scratch/Final_KSP_Project_Report_BBHs_Gokul.pdf">Gokularamanan&apos;s Report</Link></li>
-                    <li><Link href="/KSPReports/2024/Binary black holes from scratch/BH_Binary_Mergers_Report_SriramR.pdf">Sriram&apos;s Report</Link></li>
-                </ul>
-                <li><h4>Pinpricks on the vortex -  unveiling nearby galaxy evolution with star clusters</h4></li>
-                <ul>
-                    <li><Link href="/KSPReports/2024/Pinpricks on the vortex -  unveiling nearby galaxy evolution with star clusters/Star Cluster Analysis - Report.pdf">Manohara&apos;s Report</Link></li>
-                    <li><Link href="/KSPReports/2024/Pinpricks on the vortex -  unveiling nearby galaxy evolution with star clusters/ksp_project_report (4).pdf">Nishad&apos;s Report</Link></li>
-                    <li><Link href="/KSPReports/2024/Pinpricks on the vortex -  unveiling nearby galaxy evolution with star clusters/KSP_REPORT.pdf">Sanidhya&apos;s Report</Link></li>
-                    <li><Link href="/KSPReports/2024/Pinpricks on the vortex -  unveiling nearby galaxy evolution with star clusters/Final Report KSP.pdf">Soham&apos;s Report</Link></li>
-                    <li><Link href="/KSPReports/2024/Pinpricks on the vortex -  unveiling nearby galaxy evolution with star clusters/SuryaVinodEndtermReport.pdf">Surya&apos;s Report</Link></li>
-                </ul>
-                <li><h4>Radio Astronomy</h4></li>
-                <ul>
-                    <li><Link href="/KSPReports/2024/Radio Astronomy/KSP_Endterm_Report.pdf">Manit&apos;s Report</Link></li>
-                    <li><Link href="/KSPReports/2024/Radio Astronomy/Final_report.pdf">Niral&apos;s Report</Link></li>
-                    <li><Link href="/KSPReports/2024/Radio Astronomy/report.pdf">Nirav&apos;s Report</Link></li>
-                    <li><Link href="/KSPReports/2024/Radio Astronomy/Saatwik_KSP_Final.pdf">Saatwik&apos;s Report</Link></li>
-                </ul>
-                <li><h4>Study of the Nearest Hydrogen-Rich Core-Collapse Supernova: SN 2023ixf</h4></li>
-                <ul>
-                    <li><Link href="/KSPReports/2024/Study of the Nearest Hydrogen-Rich Core-Collapse Supernova SN 2023ixf/ksp_endterm.pdf">Aditi&apos;s Report</Link></li>
-                    <li><Link href="/KSPReports/2024/Study of the Nearest Hydrogen-Rich Core-Collapse Supernova SN 2023ixf/KSP_Project_Report_Divyansh_Tripathi.pdf">Divyansh&apos;s Report</Link></li>
-                    <li><Link href="/KSPReports/2024/Study of the Nearest Hydrogen-Rich Core-Collapse Supernova SN 2023ixf/Krittika.pdf">Gaurav&apos;s Report</Link></li>
-                    <li><Link href="/KSPReports/2024/Study of the Nearest Hydrogen-Rich Core-Collapse Supernova SN 2023ixf/KSP_study_of_sn2023ixf_Report.pdf">Jigyasa&apos;s Report</Link></li>
-                    <li><Link href="/KSPReports/2024/Study of the Nearest Hydrogen-Rich Core-Collapse Supernova SN 2023ixf/Report_Mayank_Soni.pdf">Mayank&apos;s Report</Link></li>
-                </ul>
-            </ul>
-            <h2>KSP 4.0</h2>
-            <p>For the summer of 2023, the projects were carried on with effort, energy and enthusiasm. The details can be found in 
-                <Link href='https://bit.ly/ksp4info'> this google doc.</Link> Take a look at the project reports:</p>
-            <ul>
-                <li><h4><Link href="/KSPReports/2023/UVIT_Report.pdf">The Dance of Death: Magnetic Cataclysmic Variables</Link></h4></li>
-                <li><h4><Link href="/KSPReports/2023/Gaia/Gaia_Report_2023 (1).pdf">Canoeing through the Stars on Gaia</Link></h4></li>
-                <li><h4>Faintest of the Brightest- GRB Hunters</h4></li>
-                
-                <ul>
-                    <li><Link href="/KSPReports/2023/GRB/GRB_Ananya.pdf">Ananya&apos;s Report</Link></li>
-                    <li><Link href="/KSPReports/2023/GRB/GRB_Ravi.pdf">Ravi&apos;s Report</Link></li>
-                    <li><Link href="/KSPReports/2023/GRB/GRB_Shreyas.pdf">Shreyas&apos;s Report</Link></li>
-                    <li><Link href="/KSPReports/2023/GRB/GRB Project Report Rahul.pdf">Rahul&apos;s Report</Link></li>
-                    <li><Link href="/KSPReports/2023/GRB/KSP_Endterm_Project_Report_Yash.pdf">Yash&apos;s Report</Link></li>
-                    <li><Link href="/KSPReports/2023/GRB/ksp_sarvesh-1.pdf">Sarvesh&apos;s Report</Link></li>
-                    <li><Link href="/KSPReports/2023/GRB/SohamMitraReportGRBHunters.pdf">Soham&apos;s Report</Link></li>
-                </ul>
-                <li><h4>Solar System Dynamics</h4></li>
-                <ul>
-                    <li><Link href="/KSPReports/2023/solar_system/KSP_Report_Suryansh_Srijan.pdf">Suryansh Srijan&apos;s Report</Link></li>
-                    <li><Link href="/KSPReports/2023/solar_system/KSP 4.0 final report.pdf">Suryansh Patidar&apos;s Report</Link></li>
-                    <li><Link href="/KSPReports/2023/solar_system/Krittika_Final_Report.pdf">Atharv&apos;s Report</Link></li>
-                    <li><Link href="/KSPReports/2023/solar_system/kspfinal.pdf">Aditya&apos;s Report</Link></li>
-                    <li><Link href="/KSPReports/2023/solar_system/The_Legrand_Orange_Book_Template__English_ (8).pdf">Ramesh&apos;s Report</Link></li>
-                </ul>
-                <li><h4>Exploring the Radio Sky</h4></li>
-                <ul>
-                    <li><Link href="/KSPReports/2023/Radio/KSP Radio Report Aadyot Bhardwaj.pdf">Aadyot&apos;s Report</Link></li>
-                    <li><Link href="/KSPReports/2023/Radio/KSP_Radio_Astro_Report (2).pdf">Atreyi&apos;s Report</Link></li>
-                    <li><Link href="/KSPReports/2023/Radio/KSP_Radio_Report___Karthik.pdf">Karthik&apos;s Report</Link></li>
-                    <li><Link href="/KSPReports/2023/Radio/ksp.pdf">Ambica&apos;s Report</Link></li>
-                    <li><Link href="/KSPReports/2023/Radio/Radio_KSP_Report.pdf">Mehul&apos;s Report</Link></li>
-                    <li><Link href="/KSPReports/2023/Radio/Report.pdf">Darsh&apos;s Report</Link></li>
-                </ul>
-            </ul>
-            <h2>KSP 3.0</h2>
-            <p>As we were moving back to an offline campus, KSP continued with full vigour. Here&apos;s a look at the reports of the projects done:</p>
-            <ul>
+export default function Resources() {
+    return(
+        <div>
+            <div className="mt-4" style={{maxWidth: "50rem", margin: "3rem auto 1rem", paddingLeft:"20px",paddingRight:"20px"}}>
+                <h1 style={{textAlign:"center"}}><b>Krittika Summer Projects</b></h1>
+                <br></br>
+                <p>We run a range of exciting astrophysics projects through KSP every summer. This is one of the largest student led such programs in the country</p>
+            </div>
 
-            <li><h4> <Link href='/KSPReports/2022/Gravitational_Waves_KSP_Report.pdf'>Generating Gravitational Waveforms using Numerical Relativity and Post Newtonian Terms</Link></h4></li>
-            <li><h4> <Link href='/KSPReports/2022/KSP_Stellar_Flares_2022.pdf'>Solar Flares</Link></h4> </li>
-            <li><h4> <Link href='/KSPReports/2022/Galactic Evolution.pdf'>Galactic Evolution Through N-Body Simulations</Link></h4> </li>
-            <li><h4> <Link href='/KSPReports/2022/Gaia_Report_Final.pdf'>Decoding Stellar Physics Hidden in GAIA Data</Link></h4> </li>
-            <li><h4> Estimating Age of A Cluster</h4></li>
-            
-            <p>This project had 3 reports:  </p> 
-               
-                <ul>
-                    <li><Link href='/KSPReports/2022/Anish_IITB_Krittika_SP3_0_2022_Final_Report.pdf'>Anish&apos;s Report</Link></li>    
-                     
-                    <li><Link href='/KSPReports/2022/Dhatri Dongre KSP Report.pdf'>Dhatri&apos;s Report</Link></li>    
-                    <li><Link href='/KSPReports/2022/Pal Aggarwal KSP Report.pdf'>Pal&apos;s Report</Link></li>   
-                </ul> 
-               
-                </ul>
-            <p>You can find testimonials of KSP mentees on <Link href="https://www.instagram.com/p/CrnxaW5saxw/">this post.</Link></p>
-            <h2>KSP 2.0</h2>
-            <p>KSP 2.0 marked the second year in KSP&apos;s journey. This time we had 2 collaborative projects. You can take a look at their detailed reports.</p>
-            <ul>
-                <li><h4><Link href="/KSPReports/2021/KSP_Report___Kirkwood_Gaps.pdf">Kirkwood Gaps</Link></h4></li>
-                <li><h4><Link href="/KSPReports/2021/Photometry and Supernovae.pdf">Photometry and Supernovae</Link></h4></li>
-            </ul>
-            <h2>KSP 1.0</h2>
-            <p>This marked the beginning of KSP. There was one project with the purpose of simulating an Analemma. You can take a look at its detailed report.</p>
-            <ul>
-                <li><h4><Link href="/KSPReports/2020/Analemma_Report.pdf">Analemma</Link></h4></li>
-            </ul>
+            <div className="mt-4">
+                <div className="card shadow-lg container mt-4 mb-4 aboutCard">
+                    <p className="display-4 pt-2 pb-2 text-center"></p>
+                    <div className="container">
+                        <div className="row justify-content-center">
+                            {resources.map((resource, key) => <ResourceCard name={resource.name} link={resource.link} img={resource.img} key={key}></ResourceCard>)}
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
